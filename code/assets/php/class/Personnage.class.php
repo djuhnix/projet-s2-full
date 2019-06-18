2b02a91c8101b37925dacc5cb59f9523c1108c56
Sébastien Soveaux <sebastien.soveaux@etudiant.univ-reims.fr>
2019-06-18 16:14:46 +0200

    ajout de class peut etre

---
+++ code/assets/php/class/Personnage.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sove0002
+ * Date: 18/06/2019
+ * Time: 16:05
+ */
+
+abstract class Personnage
+{
+    private $idPers ; //string
+    private $nom ; //string
+    private $bourse ;//string
+}
