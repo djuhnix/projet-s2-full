2fa30dfd0be1b734b8ade71a57108894388aa090
Clément Faure <clement.faure@etudiant.univ-reims.fr>
2019-06-19 09:28:43 +0200

    Définition de la classe Cellule et ses classes filles

---
+++ code/assets/php/class/Traversable.class.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../autoload.php";
+
+class Traversable extends Cellule
+{
+    public function __construct(Image $texture, Inventaire $objets = NULL,
+                                PersonnageNonJoueur $personnage = NULL,
+                                Ennemi $adversaire = NULL )
+    {
+        parent::__construct();
+        $this -> texture = $texture;
+        $this -> objets = $objets;
+        $this -> personnage = $personnage;
+        $this -> adversaire = $adversaire;
+    }
+}
