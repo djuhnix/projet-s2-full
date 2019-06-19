78808863dbe95cb7f1409bc32490b835ba0162a7
Stan PIQUET <stan.piquet@etudiant.univ-reims.fr>
2019-06-19 11:52:32 +0200

    Mise à jour Joueur

--- code/assets/php/class/Joueur.class.php
+++ code/assets/php/class/Joueur.class.php
@@ -1,12 +1,13 @@
 <?php
 
 require_once "Personnage.class.php";
+require_once "NiveauJoueur.class.php";
 
 class Joueur extends Personnage
 {
     private  $pointVie ; //Int
     private  $xp ; //int
-    private  $niveau ; //int
+    private  $niveau ; //NiveauJoueur
     private  $quetes ; //Quête[]
     private  $objetsEquipes ; // Equipement[]
     private  $position ; //Cellule
@@ -17,11 +18,27 @@ class Joueur extends Personnage
         parent::__construct("Angel");
         $this->pointVie = 3;
         $this->xp = 1;
-        $this->niveau = 1;
+        $this->niveau = NiveauJoueur::Niveau1;
         $this->quetes = array();
         $this->objetsEquipes = array();
         $this->position = null;
     }
+    
+    public function utiliserObjet(Consommables $item) : void {
+        $this->pointVie = $this->niveau[0];
+    }
+    
+    public function prendreObjet(Objet $item) : void {
+        echo $this->bourse->ajouterObjet($item);
+    }
 
+    
+    public function __toString() : String {
+        $statistique = "        Nom : {$this->nom}
+        Point de vie : {$this->pointVie} / {$this->niveau[0]}
+        XP : {$this->xp} / {$this->niveau[2]}
+        Inventaire : {$this->bourse}";
+        return $statistique;
+    }
     
 }
