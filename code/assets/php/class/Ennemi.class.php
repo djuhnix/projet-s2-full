6187cb1ad426791b79bdf4aef397b7977bcc83de
Clement FAURE <faur0010@iut-rcc-info.urca>
2019-06-20 10:07:02 +0200

    Documentation classes Cellule, Ennemi, NonTraversable et PNJ

--- code/assets/php/class/Ennemi.class.php
+++ code/assets/php/class/Ennemi.class.php
@@ -9,6 +9,14 @@ class Ennemi extends Personnage implements Interaction
     private $experienceRapporte;
     private $butin;
 
+    /**
+     * Ennemi constructor.
+     * @param String $newNom
+     * @param int $PV
+     * @param int $degat
+     * @param int $exp
+     * @param Objet $butin
+     */
     public function __construct(String $newNom, int $PV, int $degat, int $exp, Objet $butin )
     {
         parent::__construct($newNom);
@@ -17,11 +25,18 @@ class Ennemi extends Personnage implements Interaction
         $this -> experienceRapporte = $exp;
         $this -> butin = $butin;
     }
+
+    /**
+     * Méthode qui supprime l'ennemùi si ses points de vie sont égales à 0
+     */
     public function mourir() : void
     {
         if ($this -> pointsVie == 0)
             unlink($this);
     }
+    /**
+     * Méthode qui enlève -1 aux points de vie de l'ennemi lorsqu'il se fait attaquer par le joueur
+     */
     public function seFaireAttaquer() : void
     {
         $this -> pointsVie =- 1;
