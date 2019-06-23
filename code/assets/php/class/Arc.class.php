e31cae88f91bd3623d533fc2c08740c85127ee93
Rayan ZERMANI <rayan.zermani@etudiant.univ-reims.fr>
2019-06-23 16:37:44 +0200

    Update Arc.class.php

--- code/assets/php/class/Arc.class.php
+++ code/assets/php/class/Arc.class.php
@@ -1,13 +1,24 @@
 <?php
 require_once "Arme.class.php";
+//La classe fille nommée Arc de la classe mère qui est Arme
 class Arc extends Arme
 {
   private $Portee ;//int
+    /*
+
+  Constructeur de la classe Arc
+   * @param degats : dégats de l'arme
+   * @param Portée : portée de l'arc
+   * */
   public function __construct(string $nomEquip, string $description, int $degats, int $Portee)
   {
     parent::__construct($nomEquip,$description,$degats);
     $this->Portee=$Portee;
   }
+    /*
+    * accesseur de Portée
+    * @return la distance de dégats de l'Arc
+* */
   public function getPortee ():int
   {
     return $this->Portee;
